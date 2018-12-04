e4f11fa04ca3b9fd8d063b1a877debc89dda7ec5
xiaofan <1353815633@qq.com>
2018-12-04 22:30:47 +0800

    模块化

--- ES6/main.js
+++ ES6/main.js
@@ -2,44 +2,16 @@
  * @Author: xiaofan 
  * @Date: 2018-12-04 20:56:37 
  * @Last Modified by: xiaofan
- * @Last Modified time: 2018-12-04 22:20:28
+ * @Last Modified time: 2018-12-04 22:30:22
  */
 
 
-// 面向对象-类的继承
+import { str, obj, fn } from './modules.js';
+import foo from './modules.js';
 
-// 类的 constructor
+import { obj as oobj } from './modules.js';
 
-// class Test {
-// 	constructor(name) {
-// 		this.name = name;
-// 	}
-// 	getName() {
-// 		return this.name;
-// 	}
-// };
-
-// let test = new Test('xiaom');
-// console.log(test.getName());
-
-class Test {
-	constructor() {
-		this.name = 'xiaoming';
-	}
-	getName() {
-		return this.name;
-	}
-};
-
-class Cat extends Test {
-	constructor() {
-		super();
-		this.name = 'huli';
-	}
-};
-
-let test = new Cat();
-let tttt = new Test();
-console.log(tttt.getName());
-console.log(test.getName());
-
+console.log('name:', oobj.name);
+console.log('str:', str);
+console.log('fn:', fn);
+console.log('foo:', foo);
--- ES6/modules.js
+++ ES6/modules.js
@@ -1,7 +1,22 @@
 /*
  * @Author: xiaofan 
  * @Date: 2018-12-04 22:20:55 
- * @Last Modified by:   xiaofan 
- * @Last Modified time: 2018-12-04 22:20:55 
+ * @Last Modified by: xiaofan
+ * @Last Modified time: 2018-12-04 22:28:33
  */
 
+
+
+let str = 'string';
+
+let obj = {
+	name: 'xiaofan'
+};
+
+let fn = () => {
+	console.log('module test');	
+};
+
+export { str, obj, fn };
+
+export default { a: 1 };
