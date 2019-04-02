7dd2e398d2c90fef7f142877741499cbf32db19a
xiaofan <1353815633@qq.com>
2019-04-02 16:02:27 +0800

    this call aply bind

--- Pluralsight/webpack-starter/src/scripts/index.js
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -1,15 +1,35 @@
-/* closure 闭包 */
+/* this */
+/* call and apply */
+
+// let o = {   
+//     carId: 132,
+//     getId: function(prefix) {
+//         // return this.carId;
+//         return prefix + this.carId;
+//     }
+// };
+
+// let newCar = { carId: 999 };
 
 
+// // console.log( o.getId() );   // 132
+// // console.log( o.getId.call(newCar) );    // 999
+// console.log( o.getId.apply(newCar, ['ID: ']) );    // ID: 999
 
-let app = (function() {
-    let carId = 123;
-    let getId = function() {
-        return carId;
-    };
-    return {
-        getId: getId
-    };
-})();
 
-console.log( app.getId() );
+/* bind */
+
+let b = {
+    carId: 111,
+    getId: function() {
+        return this.carId;
+    }
+};
+
+let newCar = { carId: 888 };
+
+let newFn = b.getId.bind(newCar);
+
+console.log(b.getId() );
+console.log( newFn() );
+
