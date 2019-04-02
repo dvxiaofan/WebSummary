b4d9ca9f5b2ef72ab87e4ef9929e93082e5f11c4
xiaofan <1353815633@qq.com>
2019-04-02 15:34:42 +0800

    operators

--- Pluralsight/webpack-starter/src/scripts/index.js
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -1,12 +1,63 @@
+/*
+ * @Author: devzhang 
+ * @Date: 2019-04-02 15:10:20 
+ * @Last Modified by: devzhang
+ * @Last Modified time: 2019-04-02 15:34:09
+ */
 
-// 解构
-let carIds = [1, 3, 4, 6];
 
-let car1, remainingCars;
+/* function startCars(car1, car2, car3) {
+    console.log(car1, car2, car3);
+}
 
-[car1, ...remainingCars] = carIds;
+let carIds = [1, 2, 3];
 
-console.log(car1, remainingCars);
+startCars(...carIds);
+
+ */
+
+/* let string = NaN;
+
+console.log(typeof(string));
+ */
+
+
+/* for (let index = 0; index < 5; index++) {
+    if (index === 3) 
+    continue;
+    console.log(index);
+
+    
+    
+}
+ */
+
+/* 
+let id = 123;
+
+console.log(id === "123"); */
+
+/* let year = 1990;
+
+year <<= 1;
+
+console.log(year); // 3980 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
