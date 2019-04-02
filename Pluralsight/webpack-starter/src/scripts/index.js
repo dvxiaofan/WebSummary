ca783df96eb048dba8eefa0076e60b5a92adb724
xiaofan <1353815633@qq.com>
2019-04-02 15:01:36 +0800

    解构

---
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -0,0 +1,37 @@
+
+// 解构
+let carIds = [1, 3, 4, 6];
+
+let car1, remainingCars;
+
+[car1, ...remainingCars] = carIds;
+
+console.log(car1, remainingCars);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
