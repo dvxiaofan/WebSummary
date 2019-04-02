6e805b6d0df76a9cd5c7139df0e2b67a4becef2a
xiaofan <1353815633@qq.com>
2019-04-02 15:51:05 +0800

    closure 闭包

--- Pluralsight/webpack-starter/src/scripts/index.js
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -1,88 +1,15 @@
-/*
- * @Author: devzhang 
- * @Date: 2019-04-02 15:10:20 
- * @Last Modified by: devzhang
- * @Last Modified time: 2019-04-02 15:34:09
- */
-
-
-/* function startCars(car1, car2, car3) {
-    console.log(car1, car2, car3);
-}
-
-let carIds = [1, 2, 3];
-
-startCars(...carIds);
-
- */
-
-/* let string = NaN;
-
-console.log(typeof(string));
- */
-
-
-/* for (let index = 0; index < 5; index++) {
-    if (index === 3) 
-    continue;
-    console.log(index);
-
-    
-    
-}
- */
-
-/* 
-let id = 123;
-
-console.log(id === "123"); */
-
-/* let year = 1990;
-
-year <<= 1;
-
-console.log(year); // 3980 */
+/* closure 闭包 */
 
 
 
+let app = (function() {
+    let carId = 123;
+    let getId = function() {
+        return carId;
+    };
+    return {
+        getId: getId
+    };
+})();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log( app.getId() );
