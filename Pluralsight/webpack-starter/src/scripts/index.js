97548d8046741feb37a4012f0013e12724edb0da
xiaofan <1353815633@qq.com>
2019-04-03 22:06:09 +0800

    xhttp

--- Pluralsight/webpack-starter/src/scripts/index.js
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -8,3 +8,40 @@
 // console.log( trackCar(234) );
 // console.log( trackCar(543, 'BeiJing') );
 
+import $ from 'jquery';
+
+// let xHttp = new XMLHttpRequest();
+
+// xHttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//         console.log(this.responseText);
+//     }
+// };
+
+
+// xHttp.open('GET', 'https://5ca4b622ddab6d0014bc833b.mockapi.io/api/v1/user', true);
+// xHttp.send();
+
+// $.get("https://5ca4b622ddab6d0014bc833b.mockapi.io/api/v1/user",
+//     data => console.log('data: ', data)    
+// );
+
+
+// let promise = $.get("https://5ca4b622ddab6d0014bc833b.mockapi.io/api/v1/user");
+
+// promise.then(
+//     data => console.log('data: ', data),
+//     error => console.log('error: ', error)
+// );
+
+let user = {
+    name: 'xiaofan',
+    avatar: 'love'
+}
+
+let promise = $.post('', user);
+
+promise.then(
+    data => console.log('data: ', data),
+    error => console.log('error: ', error)
+)
