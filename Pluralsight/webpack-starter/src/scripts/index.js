18e5e35fbb5025559a83560516eaca36297c9680
xiaofan <1353815633@qq.com>
2019-04-03 09:14:05 +0800

    test

--- Pluralsight/webpack-starter/src/scripts/index.js
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -1,10 +1,10 @@
 /* Arrow Function */
 /* Default Parameters */
 
-let trackCar = function(carId, city='NY') {
-    console.log(`hello ${carId} in ${city}`);
-};
+// let trackCar = function(carId, city='NY') {
+//     console.log(`hello ${carId} in ${city}`);
+// };
 
-console.log( trackCar(234) );
-console.log( trackCar(543, 'BeiJing') );
+// console.log( trackCar(234) );
+// console.log( trackCar(543, 'BeiJing') );
 
