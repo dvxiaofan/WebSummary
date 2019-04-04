f8e4c076188d48be655916b3a2857b6870106f0b
xiaofan <1353815633@qq.com>
2019-04-04 13:46:09 +0800

    end

--- Pluralsight/webpack-starter/src/scripts/index.js
+++ Pluralsight/webpack-starter/src/scripts/index.js
@@ -1,47 +1,13 @@
-/* Arrow Function */
-/* Default Parameters */
 
-// let trackCar = function(carId, city='NY') {
-//     console.log(`hello ${carId} in ${city}`);
-// };
-
-// console.log( trackCar(234) );
-// console.log( trackCar(543, 'BeiJing') );
-
-import $ from 'jquery';
-
-// let xHttp = new XMLHttpRequest();
-
-// xHttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         console.log(this.responseText);
-//     }
-// };
+'use strict';
 
 
-// xHttp.open('GET', 'https://5ca4b622ddab6d0014bc833b.mockapi.io/api/v1/user', true);
-// xHttp.send();
+var arr = ['one', 'two', 'three'];
 
-// $.get("https://5ca4b622ddab6d0014bc833b.mockapi.io/api/v1/user",
-//     data => console.log('data: ', data)    
-// );
+Object.defineProperty(arr, 'last', {get: function() {
+    return this[this.length - 1];
+}});
 
+var last = arr.last;
 
-// let promise = $.get("https://5ca4b622ddab6d0014bc833b.mockapi.io/api/v1/user");
-
-// promise.then(
-//     data => console.log('data: ', data),
-//     error => console.log('error: ', error)
-// );
-
-// let user = {
-//     name: 'xiaofan',
-//     avatar: 'love'
-// }
-
-// let promise = $.post('', user);
-
-// promise.then(
-//     data => console.log('data: ', data),
-//     error => console.log('error: ', error)
-// )
+display(last);
