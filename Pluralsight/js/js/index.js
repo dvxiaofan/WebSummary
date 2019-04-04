0d6224a8376a3d6bab5d9f2fb2e49e9da56f7104
xiaofan <1353815633@qq.com>
2019-04-04 15:30:19 +0800

    object 没有 proto

--- Pluralsight/js/js/index.js
+++ Pluralsight/js/js/index.js
@@ -9,25 +9,10 @@ function Cat(name, color) {
 Cat.prototype.age = 3;
 
 let fluffy = new Cat('fluffy', 'white');
-let muffin = new Cat('muffin', 'brown');
 
-fluffy.age = 8;
-
-display(fluffy.age);
-display(fluffy.__proto__.age);
-display(muffin.age);
-
-display(Object.keys(fluffy));
-display(fluffy.hasOwnProperty('name'));
-
-// Cat.prototype.age = 90;
-
-// display(fluffy.age);
-// display(fluffy.__proto__.age);
-// display(Object.keys(fluffy));
-// display(fluffy.hasOwnProperty('name'));
-
-
+display(fluffy.__proto__);
+display(fluffy.__proto__.__proto__);
+display(fluffy.__proto__.__proto__.__proto__);
 
 
 
