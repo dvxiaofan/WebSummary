8f2391cd8e44378dd0cb500fc2ee733d40f138cc
xiaofan <1353815633@qq.com>
2019-04-04 13:59:21 +0800

    prototype

---
+++ Pluralsight/js/js/index.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+/* var arr = ['one', 'two', 'three'];
+
+Object.defineProperty(Array.prototype, 'last', {get: function() {
+    return this[this.length - 1];
+}});
+
+var last = arr.last;
+
+var arr2 = ['red', 'blue', 'green'];
+
+// display(arr2.last);
+
+display(Array); */
+
