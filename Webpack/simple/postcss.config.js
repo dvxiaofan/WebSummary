7ae2d6a1bf60d3d384557953913fe54cc6d7d6ad
xiaofan <1353815633@qq.com>
2019-05-24 14:35:18 +0800

    㔓项目正常

---
+++ Webpack/simple/postcss.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: DevZhang 
+ * @Date: 2019-05-24 14:27:24 
+ * @Last Modified by: DevZhang
+ * @Last Modified time: 2019-05-24 14:28:24
+ */
+
+
+module.exports = {
+    plugins: [
+        require('autoprefixer')()
+    ]
+}
